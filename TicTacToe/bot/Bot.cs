e8a34e426caa65a13cd6ee37ca5e3e44aaf639b4
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-19 22:35:18 +0300

    Add bot class

---
+++ TicTacToe/bot/Bot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.field;
+
+namespace TicTacToe.bot
+{
+    class Bot
+    {
+        GameField gameField;
+
+        public Bot(GameField gameField)
+        {
+            this.gameField = gameField;
+        }
+
+        public void makeMove()
+        {
+
+        }
+    }
+}
