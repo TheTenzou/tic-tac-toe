8ae5a103167d0c47673f4483bc509e478b578f72
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-20 12:58:39 +0300

    Add smart bot

---
+++ TicTacToe/Game/Players/Smart/MoveStrategy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Game.Players.Smart
+{
+    class MoveStrategy
+    {
+        private List<Move> listOfMoves;
+        public Player whoWins { set; get; }
+
+        public void addMove(Move move)
+        {
+            listOfMoves.Add(move);
+        }
+
+        public MoveStrategy()
+        {
+            this.whoWins = Player.NULL;
+        }
+
+        public List<Move> getMoves()
+        {
+            return listOfMoves;
+        }
+
+        public MoveStrategy clone()
+        {
+            MoveStrategy newMoveStrategy = new MoveStrategy();
+            newMoveStrategy.listOfMoves = new List<Move>(this.listOfMoves);
+            newMoveStrategy.whoWins = this.whoWins;
+            return newMoveStrategy;
+        }
+
+        public bool isBetter(MoveStrategy moveSrategy)
+        {
+            if (this.whoWins > moveSrategy.whoWins)
+            {
+                return true;
+            } 
+            else
+            {
+                return this.listOfMoves.Count < moveSrategy.listOfMoves.Count;
+            }
+        }
+    }
+}
--- TicTacToe/Game/Players/Smart/SmartBot.cs
+++ TicTacToe/Game/Players/Smart/SmartBot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TicTacToe.Game.Field;
+using TicTacToe.Game;
 
 namespace TicTacToe.Game.Players.Smart
 {
@@ -23,9 +24,46 @@ namespace TicTacToe.Game.Players.Smart
             throw new NotImplementedException();
         }
 
-        private generateMove(GameField tempField)
+        private MoveStrategy generateMove(GameField tempField, MoveStrategy st)
         {
-            
+            int size = tempField.size;
+
+            MoveStrategy result = st;
+
+            if (tempField.whoWin() == Player.NULL)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (tempField.getStatus(i, j) == Player.NULL)
+                        {
+                            GameField newTempField = tempField.colne();
+
+                            CellCoordinats cell = new CellCoordinats(i, j);
+                            Move move = new Move(player, cell);
+
+                            MoveStrategy newMoveStrategy = st.clone();
+
+                            newTempField.makeMove(move);
+                            st.addMove(move);
+
+                            MoveStrategy newResult = generateMove(newTempField, newMoveStrategy);
+
+                            if (result != null && newResult.isBetter(result))
+                            {
+                                result = newResult;
+                            }
+
+                        }
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return st;
+            }
         }
     }
 }
